8445b4c21dc7e0e492f4e027e677dfebf39a8b42
Hemkuah <65822571+Hemkuah@users.noreply.github.com>
2022-04-05 13:08:10 +0530

    Angular Practice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgApexchartsModule } from 'ng-apexcharts';
 
 import { AppComponent } from './app.component';
+import { TableComponent } from './table/table.component';
+import { Chart2Component } from './chart2/chart2.component';
+import { Chart3Component } from './chart3/chart3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent,
+    Chart2Component,
+    Chart3Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgApexchartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/chart2/chart2.component.ts
+++ src/app/chart2/chart2.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ChartComponent, ApexAxisChartSeries, ApexChart, ApexTitleSubtitle, ApexXAxis, ApexNonAxisChartSeries, ApexResponsive } from 'ng-apexcharts';
 
 export type ChartOptions = {
@@ -8,18 +8,17 @@ export type ChartOptions = {
   labels: any;
 }
 
-
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-chart2',
+  templateUrl: './chart2.component.html',
+  styleUrls: ['./chart2.component.css']
 })
-export class AppComponent  {
+export class Chart2Component implements OnInit {
   @ViewChild("chart")
   chart!: ChartComponent;
   public chartOptions! : Partial<ChartOptions> | any;
 
-  constructor(){
+  constructor() { 
     this.chartOptions = {
       series: [44, 55, 13, 43, 22],
       chart: {
@@ -40,31 +39,9 @@ export class AppComponent  {
         }
       ]
     };
-  /*  this.chartOptions = {
-      series: [44, 55, 13, 43, 22],
-      chart: {
-        type: "donut"
-      },
-      labels: ["3 Months", "6 Months", "1 Year", "2 Years", "3 Years"],
-      responsive: [
-        {
-          breakpoint: 480,
-          options: {
-            chart: {
-              width: 200
-            },
-            legend: {
-              position: "bottom"
-            }
-          }
-        }
-      ]
-    };*/
   }
 
+  ngOnInit(): void {
   }
-  
 
-
-
-
+}
